bd0764027242655f438760438f1ceaa46795b09b
Donarno Dan Baker <donarno.dan.baker@gmail.com>
2025-06-26 23:31:58 +0900

    fix merge mistake.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
 use bevy::prelude::*;
-<<<<<<< HEAD
-use bevy_egui::{EguiContextPass, EguiContexts, EguiPlugin, egui};
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(EguiPlugin {
-            enable_multipass_for_primary_context: true,
-        })
-        .add_systems(EguiContextPass, ui_example_system)
-        .run();
-}
-
-fn ui_example_system(mut contexts: EguiContexts) {
-    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
-        ui.label("world");
-    });
-=======
 use bevy_egui::{egui, EguiContextPass, EguiContexts, EguiPlugin};
 
 // This module should exist at src/easy_mark/mod.rs
@@ -67,5 +49,4 @@ fn ui_system(mut contexts: EguiContexts, mut editor_state: ResMut<EasyMarkEditor
             editor_state.editor.ui(ui);
         });
     }
->>>>>>> easy_mark_port_attempt_7
 }
